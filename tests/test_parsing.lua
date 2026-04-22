12dbed28940091e9fe769ab03c3cffcd6c503752
sumzeas <goldenboyjesse@gmail.com>
2026-04-21 21:53:16 -0700

    Added tests directory

---
+++ tests/test_parsing.lua
@@ -0,0 +1,49 @@
+local LU = require "luaunit"
+local Lex = require "lex"
+local Token = require "token"
+
+TestParse = {}
+
+function TestParse:setUp()
+    self.lex = Lex.new()
+end
+
+function TestParse:resetAndParse(input)
+    self.lex:reset()
+    self.lex.input = input
+    self.lex:read()
+end
+
+function TestParse:testSingleLineString()
+    self:resetAndParse([[
+(Hello world!)
+]])
+
+    local token = self.lex.tokens[1]
+    LU.assertEquals(token.type, Token.types["string"])
+    LU.assertEquals(token.value, "Hello world!")
+end
+
+function TestParse:testMultilineString()
+    self:resetAndParse([[
+(Hello
+world!)
+]])
+
+    local token = self.lex.tokens[1]
+    LU.assertEquals(token.type, Token.types["string"])
+    LU.assertEquals(token.value, "Hello\nworld!")
+end
+
+function TestParse:testEscapedString()
+    self:resetAndParse([[
+(Hello \
+world!)
+]])
+
+    local token = self.lex.tokens[1]
+    LU.assertEquals(token.type, Token.types["string"])
+    LU.assertEquals(token.value, "Hello world!")
+end
+
+os.exit(LU.LuaUnit.run())
