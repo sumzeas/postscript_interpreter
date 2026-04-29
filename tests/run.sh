505da630ae97c1e1a36db3e3bf4d9177f55768a0
sumzeas <goldenboyjesse@gmail.com>
2026-04-29 07:42:58 -0700

    changed 'commands' -> 'operations' for consistency

--- tests/run.sh
+++ tests/run.sh
@@ -1,8 +1,8 @@
 echo "Testing parser..."
 lua tests/test_parsing.lua
-echo "Testing arithmetic commands..."
+echo "Testing arithmetic operations..."
 lua tests/test_arithmetic.lua 
-echo "Testing comparison commands..."
+echo "Testing comparison operations..."
 lua tests/test_cmp.lua
-echo "Testing stack commands..."
+echo "Testing stack operations..."
 lua tests/test_stack.lua
