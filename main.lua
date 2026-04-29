23a82f91f7b367c28927233822dfec4c1d3e1f60
sumzeas <goldenboyjesse@gmail.com>
2026-04-29 10:09:46 -0700

    changed main to execute a statically scoped example

--- main.lua
+++ main.lua
@@ -1,14 +1,13 @@
 local Exec = require "modules.exec"
 
 Exec.run_source([=[
-5 dict begin
-    /x 10 def
-    /y 20 def
-    x y add =
-    x y mul =
-    x y gt
-        { (x is greater) = }
-        { (y is greater or equal) = }
-        ifelse
-end
-]=])
+/x 10 def
+/f { x } def
+/g {
+    1 dict begin
+        /x 20 def
+        f
+    end
+} def
+g =
+]=], { static_scope = true })
